cbe940b203ea53b63901876288e006358db2e86a
Oleg Fomin <oleg.fomin369@yandex.ru>
2022-09-01 23:28:30 +0300

    fix errors

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-dev_user="webmaster"
-dev_pass="1234" # TODO : CHANGE THIS IN REAL CASE
-db_name="wordpress"
-db_user="admin"
-db_pass="admin" # TODO : CHANGE THIS IN REAL CASE
-db_rt_pass="root" # TODO : CHANGE THIS IN REAL CASE
-wp_title="Title of website"
-wp_admin="admin"
-wp_pass="admin"
-wp_mail="admin@$(hostname)"
+dev_user=webmaster
+dev_pass=1234 # TODO : CHANGE THIS IN REAL CASE
+db_name=wordpress
+db_user=admin
+db_pass=admin # TODO : CHANGE THIS IN REAL CASE
+db_rt_pass=root # TODO : CHANGE THIS IN REAL CASE
+wp_title=Title of website
+wp_admin=admin
+wp_pass=admin
+wp_mail=admin@$(hostname)
 
 echo -e "\e[35mStart deployment LLMP stack (Linux, Lighttpd, MariaDB, PHP)\e[0m"
 
 useradd -m -s /bin/bash $dev_user
-chpasswd <<< "$dev_user:$dev_pass"
+echo "$dev_user:$dev_pass" | chpasswd
 echo -e "\e[33mAdd linux user - OK\e[0m"
 
 apt-get update > /dev/null
@@ -26,14 +26,6 @@ echo -e "\e[33mInstall simple utils - OK\e[0m"
 
 apt-get install -y lighttpd > /dev/null
 echo -e "\e[33mInstall web-sever - OK\e[0m"
-
-apt-get inslall -y php-cgi php-mysql > /dev/null
-echo -e "\e[33mInstall PHP - OK\e[0m"
-
-lighty-enable-mod fastcgi
-lighty-enable-mod fastcgi-php
-service lighttpd force-reload
-echo -e "\e[33mEnable FastCGI - OK\e[0m"
 
 apt-get install -y mariadb-server > /dev/null
 echo -e "\e[33mInstall MariaDB - OK\e[0m"
@@ -50,29 +42,36 @@ mariadb -e "GRANT ALL ON $db_name.* TO $db_user@'localhost'"
 mariadb -e "FLUSH PRIVILEGES"
 echo -e "\e[33mCreate DB and DB-user - OK\e[0m"
 
-apt-get install -y phpmyadmin
-echo -e "\e[33mInstall phpMyAdmin - OK\e[0m"
+apt-get inslall php-cgi php-mysql > /dev/null
+echo -e "\e[33mInstall PHP - OK\e[0m"
+
+lighty-enable-mod fastcgi
+lighty-enable-mod fastcgi-php
+service lighttpd force-reload
+echo -e "\e[33mEnable FastCGI - OK\e[0m"
+#apt-get install -y phpmyadmin
+#echo -e "\e[33mInstall phpMyAdmin - OK\e[0m"
 
 wget -O wp-cli https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli
 mv wp-cli /usr/local/bin
 wp-cli core download --allow-root --path="/var/www/html"
 echo -e "\e[33mInstall Wordpress - OK\e[0m"
-wp-cli config create --allow-root --path=/var/www/ --dbname=$db_name --dbuser=$db_user --dbpass=$db_pass --dbhost=localhost --dbprefix=wp_
-wp-cli core install --allow-root --path=/var/www/ --url="http://$(hostname)" --title=$wp_title --admin_user=$wp_admin --admin_password=$wp_pass --admin_email=$wp_mail --skip-email
+wp-cli config create --allow-root --path=/var/www/html --dbname=$db_name --dbuser=$db_user --dbpass=$db_pass --dbhost=localhost --dbprefix=wp_
+wp-cli core install --allow-root --path=/var/www/html --url="http://$(hostname)" --title=$wp_title --admin_user=$wp_admin --admin_password=$wp_pass --admin_email=$wp_mail --skip-email
 chown -R www-data:www-data /var/www
 echo -e "\e[33mConfig Wordpress - OK\e[0m"
 
 apt-get install -y vsftpd > /dev/null
 echo -e "\e[33mInstall FTP-server - OK\e[0m"
-mkdir -p /home/$dev_user/ftp/pub
-chown nobody:nogroup /home/$dev_user/ftp
-chmod a-w /home/$new_user/ftp
-tee -a /etc/vsftpd.conf <<< "write_enable=YES" > /dev/null
-tee -a /etc/vsftpd.conf <<< "chroot_local_user=YES" > /dev/null
-tee -a /etc/vsftpd.conf <<< "allow_writeable_chroot=YES" > /dev/null
-tee -a /etc/vsftpd.conf <<< "user_sub_token=$USER" > /dev/null
-tee -a /etc/vsftpd.conf <<< "local_root=/home/$USER/ftp" > /dev/null
+mkdir -p "/home/$dev_user/ftp/pub"
+chown nobody:nogroup "/home/$dev_user/ftp"
+chmod a-w "/home/$dev_user/ftp"
+echo -e "write_enable=YES" | tee -a /etc/vsftpd.conf  > /dev/null
+echo -e "chroot_local_user=YES" | tee -a /etc/vsftpd.conf > /dev/null
+echo -e "allow_writeable_chroot=YES" | tee -a /etc/vsftpd.conf > /dev/null
+echo -e "user_sub_token=$USER" | tee -a /etc/vsftpd.conf > /dev/null
+echo -e "local_root=/home/$USER/ftp" | tee -a /etc/vsftpd.conf > /dev/null
 service vsftpd restart
 echo -e "\e[33mConfig FTP-server - OK\e[0m"
 
